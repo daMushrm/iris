51aea8be925016ab32d8de4bc3ee6b7e991b6a8d
Mush <hilariousbananas@proton.me>
2024-06-05 18:46:58 +0300

    [+] FEAT: Add remind option

--- iris.sh
+++ iris.sh
@@ -2,14 +2,15 @@
 
 # Display usage information
 display_help() {
-    echo "Usage: $0 [help | warm | dim | inv | off]"
+    echo "Usage: $0 [help | warm | dim | inv | off | remind]"
     echo
     echo "Options:"
-    echo "  ❓    help  Show this help message and exit"
-    echo "  🌞    warm  Apply a warm color temperature to the screen"
-    echo "  🌑    dim   Make the screen darker"
-    echo "  🌈    inv   Invert the screen colors"
-    echo "  🔄    off   Return to normal screen settings"
+    echo "  ❓    help   Show this help message and exit"
+    echo "  🌞    warm   Apply a warm color temperature to the screen"
+    echo "  🌑    dim    Make the screen darker"
+    echo "  🌈    inv    Invert the screen colors"
+    echo "  🔄    off    Return to normal screen settings"
+    echo "  ⏰    remind Start a reminder every 15 minutes"
 }
 
 # Show help if -h or no argument is provided
@@ -44,6 +45,31 @@ if [ "$1" == "warm" ]; then
     xcalib -blue 1.8 0 50 -alter
     exit
 fi
+
+# Start a reminder every 15 minutes
+if [ "$1" == "remind" ]; then
+    habits=(
+        "Take some rest."
+        "walk around for a minute."
+        "Stretch for a bit."
+        "Drink some water."
+        "Adjust your posture."
+        "Look away from the screen for a minute."
+        "Take a deep breath."
+        "Close your eyes for a moment."
+        "Relax your shoulders."
+    )
+
+    while true; do
+        # Randomly select a habit from the list
+        reminder=${habits[$RANDOM % ${#habits[@]}]}
+        sleep 5
+        echo "[!] $reminder"
+        notify-send -t 5000 "Iris" "$reminder"
+    done
+    exit
+fi
+
 
 # If an unknown option is provided, show help
 display_help
